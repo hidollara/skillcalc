38de21b01a1df9dead62e1222110d39b03282121
hidollara <hidollara@users.noreply.github.com>
2018-04-17 03:13:15 +0900

    v0.4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Grid, Row, Col, Checkbox } from 'react-bootstrap';
+import { Grid, Row, Col, ControlLabel } from 'react-bootstrap';
+import CircularProgressbar from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import key from './key.png';
+
+const DIFF_MAX = 9.99;
+const DIFF_MIN = 1.00;
+const RATE_MAX = 100.00;
+const RATE_MIN = 0.01;
 
 class App extends React.Component {
   constructor(props) {
@@ -19,9 +27,9 @@ class App extends React.Component {
       this.setState({ diff: v });
       return;
     }
-    var calcRate = (this.calcSkill() * 5.0) / v;
-    if (calcRate < 0.01 || 100.00 < calcRate) return;
-    this.setState({ diff: v, rate: calcRate });
+    var rate = (this.calcSkill() * 5.0) / v;
+    if (rate < RATE_MIN || RATE_MAX < rate) return;
+    this.setState({ diff: v, rate: rate });
   }
 
   handleChangeRate(v) {
@@ -29,9 +37,9 @@ class App extends React.Component {
       this.setState({ rate: v });
       return;
     }
-    var calcDiff = (this.calcSkill() * 5.0) / v;
-    if (calcDiff < 1.00 || 9.99 < calcDiff) return;
-    this.setState({ diff: calcDiff, rate: v });
+    var diff = (this.calcSkill() * 5.0) / v;
+    if (diff < DIFF_MIN || DIFF_MAX < diff) return;
+    this.setState({ diff: diff, rate: v });
   }
 
   calcSkill() {
@@ -41,44 +49,57 @@ class App extends React.Component {
   render() {
     return (
       <Grid>
-        <Row>
-          <Col xs={12} md={6} mdOffset={3}>
-            <Checkbox
-              checked={this.state.isSkillFixed}
-              onChange={e => this.setState({ isSkillFixed: e.target.checked })}
-            >
-              {this.calcSkill().toFixed(2)}
-            </Checkbox>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} md={1} mdOffset={3}>
-            {this.state.diff.toFixed(2)}
-          </Col>
-          <Col xs={12} md={5}>
-            <Slider
-              value={this.state.diff}
-              min={1.00}
-              max={9.99}
-              step={0.05}
-              onChange={this.handleChangeDiff.bind(this)}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} md={1} mdOffset={3}>
-            {this.state.rate.toFixed(2)}
-          </Col>
-          <Col xs={12} md={5}>
-            <Slider
-              value={this.state.rate}
-              min={0.01}
-              max={100.00}
-              step={0.01}
-              onChange={this.handleChangeRate.bind(this)}
-            />
-          </Col>
-        </Row>
+        <Col xs={12} sm={6}>
+          <Row>
+            <Col xs={10} xsOffset={1}>
+              <ControlLabel>
+                <div style={{ position: "relative" }}>
+                  <input type="checkbox"
+                    checked={this.state.isSkillFixed}
+                    onChange={e => {
+                      this.setState({ isSkillFixed: e.target.checked });
+                    }}
+                    style={{ display: "none" }}
+                  />
+                  <img src={key} alt={"Locked"} />
+                  <CircularProgressbar
+                    percentage={100 * this.calcSkill() / 170.0}
+                    textForPercentage={_ => this.calcSkill().toFixed(2)}
+                  />
+                </div>
+              </ControlLabel>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={12} sm={6}>
+          <Row>
+            <Col xs={12}>
+              {this.state.diff.toFixed(2)}
+            </Col>
+            <Col xs={12}>
+              <Slider
+                value={this.state.diff}
+                min={DIFF_MIN}
+                max={DIFF_MAX}
+                step={0.05}
+                onChange={this.handleChangeDiff.bind(this)}
+              />
+            </Col>
+            <Col xs={12}>
+              {this.state.rate.toFixed(2)}
+            </Col>
+            <Col xs={12}>
+              <Slider
+                value={this.state.rate}
+                min={RATE_MIN}
+                max={RATE_MAX}
+                step={0.01}
+                marks={{63: "B", 73: "A", 80: "S", 95: "SS"}}
+                onChange={this.handleChangeRate.bind(this)}
+              />
+            </Col>
+          </Row>
+        </Col>
       </Grid>
     );
   }
